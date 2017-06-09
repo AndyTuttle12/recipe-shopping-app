af2c54e10d479ddaf12a0e5e0de1ae07a4d7dcfd
Andy Tuttle <andytuttle12@gmail.com>
2017-06-08 22:30:05 -0400

    Fixed auth bugs and added basic token auth from firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import * as firebase from 'firebase';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyAnZaHpyuQ_b9OSi8UD1sDdZh0QCTsJZus",
-      authDomain: "ng-recipe-book-5915c.firebaseapp.com"
+      apiKey: "AIzaSyA5GxIRegv_FFkUtQr2e9zDercSW8Mo5L8",
+      authDomain: "recipe-book-app-cc13f.firebaseapp.com"
     });
   }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,17 +4,25 @@ import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-5915c.firebaseio.com//recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://recipe-book-app-cc13f.firebaseapp.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book-5915c.firebaseio.com/.com/recipes.json')
+    const token = this.authService.getToken();
+
+    this.http.get('https://recipe-book-app-cc13f.firebaseapp.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
@@ -23,7 +31,6 @@ export class DataStorageService {
               recipe['ingredients'] = [];
             }
           }
-          console.log(recipes)
           return recipes;
         }
       )
